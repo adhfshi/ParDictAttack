6186b2f94448b9f70ef953757df63c3a10135755
haofan <shi.hao@husky.neu.edu>
2016-02-23 17:52:51 -0500

    update1 v1.0

---
+++ hash_gen.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "sha256.h"
+
+void hashgen(unsigned char text[], unsigned char hash[]){	
+	SHA256_CTX ctx;
+	
+	sha256_init(&ctx);
+	sha256_update(&ctx,text,strlen((const char*)text));
+	sha256_final(&ctx,hash);
+	
+}
+
+int main(){
+	unsigned char text1[]={"redsox29"},
+				  text2[]={"zipcar22"},
+				  text3[]={"husky"},
+				  text4[]={"chicken92"},
+				  text5[]={"Eis9Xi99asd"},
+				  hash[32];
+
+	FILE *fp = fopen("hash_value.dat","w+");
+
+	hashgen(text1,hash);
+	fwrite(hash, sizeof(char), sizeof(hash), fp);
+	memset((void*)&hash[0], 0, 32);
+	hashgen(text2,hash);
+	fwrite(hash, sizeof(char), sizeof(hash), fp);
+	memset((void*)&hash[0], 0, 32);
+	hashgen(text3,hash);
+	fwrite(hash, sizeof(char), sizeof(hash), fp);
+	memset((void*)&hash[0], 0, 32);
+	hashgen(text4,hash);
+	fwrite(hash, sizeof(char), sizeof(hash), fp);
+	memset((void*)&hash[0], 0, 32);
+	hashgen(text5,hash);
+	fwrite(hash, sizeof(char), sizeof(hash), fp);
+	memset((void*)&hash[0], 0, 32);
+	fclose(fp);
+
+}
